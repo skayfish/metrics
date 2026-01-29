654ea9da68a44e9fb07a7da64e393ffb43939a81
skayfish <e.zainullin2@yandex.ru>
2026-01-30 01:37:01 +0500

    ref: check connections with server

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -23,6 +23,12 @@ const host string = "localhost"
 // SF TODO
 const port string = "8080"
 
+// SF TODO
+const timeout time.Duration = 30 * time.Second
+
+// SF TODO
+const waitInterval time.Duration = 2 * time.Second
+
 // PingTCP проверяет, можно ли подключиться к хосту:порту за указанное время
 func PingTCP(host string, port string, timeout time.Duration) error {
 	address := net.JoinHostPort(host, port)
@@ -35,14 +41,36 @@ func PingTCP(host string, port string, timeout time.Duration) error {
 }
 
 // SF TODO
-func main() {
+func checkConnection() bool {
 	// Проверка соединения с сервером
-	timeout := 30 * time.Second
-	err := PingTCP(host, port, timeout)
-	if err != nil {
-		fmt.Printf("Не удалось подключиться к %s:%s: %v\n\n\n", host, port, err)
-	} else {
-		fmt.Printf("Успешно подключились к %s:%s\n\n\n", host, port)
+	totalTime := time.Duration(0)
+	totalAttempts := timeout.Nanoseconds() / waitInterval.Nanoseconds()
+	attemptsCounter := 1
+	for {
+		if totalTime >= timeout {
+			fmt.Printf("Не удалось подключиться к %s:%s ;(\n", host, port)
+			return false
+		}
+
+		err := PingTCP(host, port, timeout)
+		if err != nil {
+			fmt.Printf("Не удалось подключиться к %s:%s: %v\n", host, port, err)
+			fmt.Printf("Жду 2 секунды... [%d/%d]\n", attemptsCounter, totalAttempts)
+			attemptsCounter++
+		} else {
+			fmt.Printf("Успешно подключились к %s:%s\n\n", host, port)
+			return true
+		}
+
+		time.Sleep(2 * time.Second)
+		totalTime += 2 * time.Second
+	}
+}
+
+// SF TODO
+func main() {
+	if !checkConnection() {
+		return
 	}
 
 	// Запуск отправки метрик
