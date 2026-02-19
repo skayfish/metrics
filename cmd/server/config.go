f60e24b90a9fc5f7b8d053138c441eff278d0b86
skayfish <e.zainullin2@yandex.ru>
2026-02-19 16:31:52 +0500

    add: parse config

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/caarlos0/env/v6"
 	"github.com/skayfish/metrics/internal/flags"
@@ -14,6 +15,15 @@ import (
 type environments struct {
 	// Сетевой адрес
 	Address *flags.NetAddress `env:"ADDRESS" example:"localhost:8080"`
+
+	// SF TODO
+	StoreInterval *uint `env:"STORE_INTERVAL" example:"5"`
+
+	// SF TODO
+	FileStoragePath *string `env:"FILE_STORAGE_PATH" example:"/home/user/server"`
+
+	// SF TODO
+	ToRestore *bool `env:"RESTORE" example:"true"`
 }
 
 // Парсит флаги, указанные при запуске программы и переменные окружения
@@ -26,6 +36,12 @@ func parseConfig() (*server.Config, error) {
 	pflag.VarP(&addr, "address", "a", "Server listening address in host:port format")
 	var logLevel logger.Level
 	pflag.VarP(&logLevel, "log-level", "l", "Logging level")
+	storeInterval := pflag.UintP("store-interval", "i", 300,
+		"Number of seconds before current storage data is written to the \"--file-storage-path\" location")
+	fileStoragePath := pflag.StringP("file-storage-path", "f", ".",
+		"File system path to which current storage data is persisted")
+	toRestore := pflag.BoolP("restore", "r", false,
+		"Read saved values from the \"--file-storage-path\" file when the server starts (default false)")
 
 	pflag.Parse()
 
@@ -33,16 +49,31 @@ func parseConfig() (*server.Config, error) {
 	var envs environments
 	err := env.Parse(&envs)
 	if err != nil {
-		return nil, fmt.Errorf("main: failed to parse environment variables: %v", err)
+		return nil, fmt.Errorf("failed to parse environment variables: %v", err)
 	}
 
 	if envs.Address != nil {
 		addr = *envs.Address
 	}
 
+	if envs.StoreInterval != nil {
+		*storeInterval = *envs.StoreInterval
+	}
+
+	if envs.FileStoragePath != nil {
+		*fileStoragePath = *envs.FileStoragePath
+	}
+
+	if envs.ToRestore != nil {
+		*toRestore = *envs.ToRestore
+	}
+
 	result := server.Config{
-		Address:  addr,
-		LogLevel: logLevel,
+		Address:         addr,
+		LogLevel:        logLevel,
+		StoreInterval:   time.Duration(*storeInterval) * time.Second,
+		FileStoragePath: *fileStoragePath,
+		ToRestore:       *toRestore,
 	}
 
 	return &result, nil
--- internal/server/config.go
+++ internal/server/config.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"time"
+
 	"github.com/skayfish/metrics/internal/flags"
 	"github.com/skayfish/metrics/internal/logger"
 )
@@ -12,4 +14,13 @@ type Config struct {
 
 	// Уровень логирования
 	LogLevel logger.Level
+
+	// SF TODO
+	StoreInterval time.Duration
+
+	// SF TODO
+	FileStoragePath string
+
+	// SF TODO
+	ToRestore bool
 }
