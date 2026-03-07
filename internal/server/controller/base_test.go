e70b0199d8a6ead6395c3c00ce3589daa5ba8ef0
skayfish <e.zainullin2@yandex.ru>
2026-03-07 13:30:45 +0500

    add: test ping

--- internal/server/controller/base_test.go
+++ internal/server/controller/base_test.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"context"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -9,10 +10,25 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-resty/resty/v2"
 	"github.com/golang/mock/gomock"
+	"github.com/skayfish/metrics/internal/model"
 	"github.com/skayfish/metrics/internal/server/storage"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+type UnknownStorage struct{}
+
+func (UnknownStorage) Update(m model.Metrics) (*model.Metrics, error) { return nil, nil }
+func (UnknownStorage) UpdateContext(ctx context.Context, m model.Metrics) (*model.Metrics, error) {
+	return nil, nil
+}
+func (UnknownStorage) Get(id string) (*model.Metrics, error) { return nil, nil }
+func (UnknownStorage) GetContext(ctx context.Context, id string) (*model.Metrics, error) {
+	return nil, nil
+}
+func (UnknownStorage) GetAll() ([]model.Metrics, error)                           { return nil, nil }
+func (UnknownStorage) GetAllContext(ctx context.Context) ([]model.Metrics, error) { return nil, nil }
+func (UnknownStorage) Close() error                                               { return nil }
 
 // Проверяет работу обработчика запроса на проверку соединения
 func TestBaseController_Ping(t *testing.T) {
@@ -22,12 +38,12 @@ func TestBaseController_Ping(t *testing.T) {
 		statusCode    int
 	}{
 		{
-			test:          "success",
+			test:          "database: success",
 			databaseError: nil,
 			statusCode:    http.StatusOK,
 		},
 		{
-			test:          "error",
+			test:          "database: error",
 			databaseError: errors.New("some error"),
 			statusCode:    http.StatusInternalServerError,
 		},
@@ -55,4 +71,38 @@ func TestBaseController_Ping(t *testing.T) {
 			assert.Equal(t, "", resp.String())
 		})
 	}
+
+	t.Run("memory storage", func(t *testing.T) {
+		storage := storage.NewMemStorage()
+		controller := NewBaseController(&storage)
+
+		router := chi.NewRouter()
+		router.Get("/ping", controller.Ping)
+		server := httptest.NewServer(router)
+		defer server.Close()
+
+		request := resty.New().R()
+		resp, err := request.Get(server.URL + "/ping")
+		require.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode())
+		assert.Equal(t, "", resp.String())
+	})
+
+	t.Run("unknown storage", func(t *testing.T) {
+		storage := UnknownStorage{}
+		controller := NewBaseController(&storage)
+
+		router := chi.NewRouter()
+		router.Get("/ping", controller.Ping)
+		server := httptest.NewServer(router)
+		defer server.Close()
+
+		request := resty.New().R()
+		resp, err := request.Get(server.URL + "/ping")
+		require.NoError(t, err)
+
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode())
+		assert.Equal(t, "", resp.String())
+	})
 }
