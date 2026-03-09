3cf5b12fb3a3c4822cb2bdbf30a75eaa69f4ae8f
skayfish <e.zainullin2@yandex.ru>
2026-03-09 08:32:30 +0500

    ref: agent for /updates

--- cmd/server/config_test.go
+++ cmd/server/config_test.go
@@ -17,7 +17,6 @@ func Test_parseConfig(t *testing.T) {
 	t.Run("default values", func(t *testing.T) {
 		config, err := parseConfig()
 		require.NoError(t, err)
-		require.NoError(t, err)
 		assert.Equal(t, server.Config{
 			Address: flags.NetAddress{
 				Host: "localhost",
